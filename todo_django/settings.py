e19efc38974f9f5e4bffcf20933bbe1ddeee9104
Rajvi Zala <89260078+rajvizala@users.noreply.github.com>
2023-01-29 18:14:17 +0530

    Update settings.py

--- todo_django/settings.py
+++ todo_django/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '8vzxz_!m6ta0wuumui4^mo@&c!a#y02(ao8+29s655z^50!@ul'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
